0550890a28307fbdb57c9579d77a7a1ca9255d21
Tien Nguyen <htien@users.noreply.github.com>
2017-03-29 18:44:06 +0700

    Upgrade the project to 5.4 from 5.3.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,7 +34,7 @@
 <meta property="og:locale" content="en_US" />
 <meta property="og:locale:alternate" content="vi_VN" />
 <meta property="og:url" content="https://tnguyen.net" />
-<meta property="og:image" content="https://tnguyen.net/image.png" />
+<meta property="og:image" content="https://tnguyen.net/images/avatar.png" />
 <meta property="og:image:type" content="image/png" />
 <meta property="og:image:width" content="500" />
 <meta property="og:image:height" content="500" />
@@ -75,26 +75,25 @@
 
 {{-- Relationship: Apple Touch Icons --}}
 
-<link rel="apple-touch-icon" href="apple-icon.png" />
-<link rel="apple-touch-icon" sizes="57x57" href="apple-icon-57x57.png" />
-<link rel="apple-touch-icon" sizes="60x60" href="apple-icon-60x60.png" />
-<link rel="apple-touch-icon" sizes="72x72" href="apple-icon-72x72.png" />
-<link rel="apple-touch-icon" sizes="76x76" href="apple-icon-76x76.png" />
-<link rel="apple-touch-icon" sizes="114x114" href="apple-icon-114x114.png" />
-<link rel="apple-touch-icon" sizes="120x120" href="apple-icon-120x120.png" />
-<link rel="apple-touch-icon" sizes="144x144" href="apple-icon-144x144.png" />
-<link rel="apple-touch-icon" sizes="152x152" href="apple-icon-152x152.png" />
-<link rel="apple-touch-icon" sizes="180x180" href="apple-icon-180x180.png" />
+<link rel="apple-touch-icon" href="images/apple-icon.png" />
+<link rel="apple-touch-icon" sizes="57x57" href="images/apple-icon-57x57.png" />
+<link rel="apple-touch-icon" sizes="60x60" href="images/apple-icon-60x60.png" />
+<link rel="apple-touch-icon" sizes="72x72" href="images/apple-icon-72x72.png" />
+<link rel="apple-touch-icon" sizes="76x76" href="images/apple-icon-76x76.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="images/apple-icon-114x114.png" />
+<link rel="apple-touch-icon" sizes="120x120" href="images/apple-icon-120x120.png" />
+<link rel="apple-touch-icon" sizes="144x144" href="images/apple-icon-144x144.png" />
+<link rel="apple-touch-icon" sizes="152x152" href="images/apple-icon-152x152.png" />
+<link rel="apple-touch-icon" sizes="180x180" href="images/apple-icon-180x180.png" />
 
 {{-- Relationship: Icons --}}
 
-<link rel="icon" type="image/png" sizes="192x192" href="android-icon-192x192.png" />
-<link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
-<link rel="icon" type="image/png" sizes="96x96" href="android-icon-96x96.png" />
-<link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
-<link rel="manifest" href="manifest.json" />
+<link rel="icon" type="image/png" sizes="96x96" href="images/favicon-96x96.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png" />
+<link rel="manifest" href="images/manifest.json" />
 <meta name="msapplication-TileColor" content="#ffffff" />
-<meta name="msapplication-TileImage" content="ms-icon-144x144.png" />
+<meta name="msapplication-TileImage" content="images/ms-icon-144x144.png" />
 <meta name="theme-color" content="#0e0529" />
 
 {{-- Stylesheet --}}
